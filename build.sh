de355cf11729faf14e364019bdc5b3e197ea424f
mahmoudmohamed57 <mahmoudmohamed752000@gmail.com>
2024-04-21 22:51:11 +0200

    update build.sh and store app

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+# Inserting data in postgresql
+python manage.py seed_db
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
